40a619487c06c6e9c90be1cf5cd4638b2a2bbaf0
wokanjianyang <569284543@qq.com>
2024-05-09 23:08:28 +0800

    temp

--- Code/Assets/Scripts/Player/CircleProgress.cs
+++ Code/Assets/Scripts/Player/CircleProgress.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game
 {
     public class CircleProgress : MonoBehaviour
     {
-        public Material material;
+        public Image image;
 
         // Start is called before the first frame update
         void Start()
@@ -21,11 +22,7 @@ namespace Game
 
         public void SetPercent(float percent)
         {
-            Debug.Log("Set Begin:" + material.GetFloat("_Fill"));
-
-            material.SetFloat("_Fill", percent);
-
-            Debug.Log("Set Over:" + material.GetFloat("_Fill"));
+            image.fillAmount = percent;
         }
     }
 }

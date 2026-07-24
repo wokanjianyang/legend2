b76f831f76a4108a87a58fff188f9f398b8703a4
wokanjianyang <569284543@qq.com>
2026-07-24 17:07:46 +0800

    t

--- Code/Assets/Scripts/View_Bag.cs
+++ Code/Assets/Scripts/View_Bag.cs
@@ -123,7 +123,11 @@ namespace Game
         void Start()
         {
             User user = User_Data_Manager.Data;
-            if (user.AccountId == "" || user.MagicLevel.Data <= 20)
+            if (ConfigHelper.Channel == ConfigHelper.Channel_Tap || user.AccountId == "" || user.MagicLevel.Data <= 20)
+            {
+                this.Btn_Store.gameObject.SetActive(false);
+            }
+            else
             {
                 this.Btn_Store.gameObject.SetActive(false);
             }

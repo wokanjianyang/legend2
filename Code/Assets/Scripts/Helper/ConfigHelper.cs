f848bd3ae6c7502f65390573024233384bab9210
569284543@qq.com <569284543@qq.com>
2026-08-19 11:59:08 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -26,7 +26,7 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 107;
+        public const int Version = 108;
 
         public const long PackTime = 1786673429; //打包时间，防止作弊
 

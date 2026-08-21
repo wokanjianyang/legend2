5b2ad3dff427c698c1ff038a85806699736b2018
wokanjianyang <569284543@qq.com>
2026-08-21 11:59:11 +0800

    t

--- Code/Assets/Plugins/Android/java/com/unity3d/player/MainActivity.java
+++ Code/Assets/Plugins/Android/java/com/unity3d/player/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends Activity implements PermissionsUtils.IPermissi
                 Manifest.permission.INTERNET,
 //                 Manifest.permission.READ_PRECISE_PHONE_STATE,
 //                 Manifest.permission.ACCESS_NETWORK_STATE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
 //                 Manifest.permission.ACCESS_WIFI_STATE,
 //                 Manifest.permission.ACCESS_COARSE_LOCATION,
 //                 Manifest.permission.REQUEST_INSTALL_PACKAGES,
